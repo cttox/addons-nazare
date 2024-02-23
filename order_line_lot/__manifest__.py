75f3217de8f8a4995b2244e478b5d441e40261b8
cttox <cesar.barron@gmail.com>
2024-02-23 10:35:48 -0600

    [DEV] Module to retrieve lot and date of lot in the sale order line when a product is selected

---
+++ order_line_lot/__manifest__.py
@@ -0,0 +1,17 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Order line lot',
+    'version': '1.0',
+    'category': 'Sales/Sales',
+    'summary': '',
+    'description': """""",
+    'author': "",
+    'depends': ['sale'],
+    'data': [
+    ],
+    'assets': {
+    },
+    'application': True,
+    'license': 'LGPL-3'
+}
